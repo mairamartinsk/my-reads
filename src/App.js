f9e863efcf283d08d6f4163ce8129fc085259722
Maira K <mairamartinsk@gmail.com>
2018-07-01 16:19:29 +0200

    Fix map books method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
-import "./App.css";
-
 import SearchPage from "./SearchPage";
 import Bookshelf from "./Bookshelf";
+import "./App.css";
 
 class BooksApp extends React.Component {
   state = {
