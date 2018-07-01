3268b7224295294111228de751e8bec6547bee5d
Maira K <mairamartinsk@gmail.com>
2018-07-01 15:51:04 +0200

    Pass props to new component SearchResults

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,22 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import * as BooksAPI from "./BooksAPI";
-import "./App.css";
+import SearchResults from "./SearchResults";
 
 class SearchPage extends React.Component {
+  clearQueryInput() {
+    this.setState({
+      query: ""
+    });
+  }
+
   render() {
+    const { state, update, search } = this.props;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link to="/" className="close-search">
+          <Link to="/" className="close-search" onClick={this.clearQueryInput}>
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={state.query}
+              onChange={event => search(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <SearchResults
+            books={state.books}
+            results={state.results}
+            query={state.query}
+            onChange={update}
+          />
         </div>
       </div>
     );
